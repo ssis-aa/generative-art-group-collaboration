bb327fc9f81f2e8cafda3ad67872004a5c555b52
Matthias Kreier <98946372+matthiaskreier@users.noreply.github.com>
2022-11-08 14:31:42 +0700

    Create art.js

---
+++ art.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+  // Draw a circle at location (30, 30) with a diameter of 20.
+  circle(30, 30, 20);
+}
+
+function draw() {
+  //background(220);
+}
