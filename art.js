57183dc44681eae49277ca0d7c5c84d2feb478ba
dongdongthedingdong <111406269+dongdongthedingdong@users.noreply.github.com>
2022-11-08 14:34:21 +0700

    Update art.js
    Changed the background to red

--- art.js
+++ art.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(400, 400);
-  background(220);
+  background(220, 0, 0);
   // Draw a circle at location (30, 30) with a diameter of 20.
   circle(30, 30, 20);
   // Change by Dan
