062854139ee8dfd5765d9bcd9251dc44378794c7
Matthias Kreier <98946372+matthiaskreier@users.noreply.github.com>
2022-11-08 14:55:49 +0700

    Update art.js

--- art.js
+++ art.js
@@ -1,13 +1,12 @@
 function setup() {
   createCanvas(400, 400);
-  background(0, 0, 0);
+  background(220);
   // Draw a circle at location (30, 30) with a diameter of 20.
-  circle(30, 30, 20)
+  circle(30, 30, 20);
   
-
-
   // Change by Dan
   rect(50, 50, 55, 55);
+  
   // Change by Khang
   let c1 = color(0,0,255)
   fill(c1)
@@ -15,10 +14,24 @@ function setup() {
   let c2 = color(255,0,0)
   fill(c2)
   triangle(400, 400, 200, 50, 0, 400);
+  
   // Change by William
+  
   
   // Change by Dongjae
   
+  
+  // Change by mk
+  dx = 300
+  dy = 150
+  line(30 + dx, 20 + dy, 85 + dx, 20 + dy);
+  stroke(126);
+  line(85 + dx, 20 + dy, 85 + dx, 75 + dy);
+  stroke(255);
+  line(85 + dx, 75 + dy, 30 + dx, 75 + dy);
+  noStroke()
+  fill(color(77,220,77));
+  rect(30 + dx, 21 + dy, 53, 53);
 }
 
 function draw() {
